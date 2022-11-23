d74b51403a44ce1387d1eae4668a2131be13c809
Lazar <65450353+la-ki@users.noreply.github.com>
2022-11-23 14:58:40 +0100

    Update producDataRequest.js

--- requests/products/producDataRequest.js
+++ requests/products/producDataRequest.js
@@ -2,7 +2,7 @@ import apiEndpoints from '../apiEndpoints';
 
 export const getProductData = async (productId) => {
   const response = await fetch(
-    `http://localhost:3000${apiEndpoints.products}/${productId}`
+    `${apiEndpoints.products}/${productId}`
   );
 
   const data = await response.json();
