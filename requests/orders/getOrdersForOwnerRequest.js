369cf4bb450b773663f78556d7d4ce89f9868bca
Lazar <65450353+la-ki@users.noreply.github.com>
2022-11-23 14:57:35 +0100

    Update getOrdersForOwnerRequest.js

--- requests/orders/getOrdersForOwnerRequest.js
+++ requests/orders/getOrdersForOwnerRequest.js
@@ -2,7 +2,7 @@ import apiEndpoints from '../apiEndpoints';
 
 export const getOrdersForOwner = async (id) => {
   const response = await fetch(
-    `http://localhost:3000${apiEndpoints.order}?ownerID=${id}`
+    `${apiEndpoints.order}?ownerID=${id}`
   );
 
   const data = await response.json();
